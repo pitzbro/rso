6e7f8d358a7d963694b9d6b60b6be8a3edc1cd31
pitzbro <asioren@gmail.com>
2017-11-22 20:02:06 +0200

    fixed search

--- toggleSearch.js
+++ toggleSearch.js
@@ -1,6 +1,17 @@
 function toggleSearch() {
     var searchBlock = document.getElementById('block-views-exp-search-page');
+    var searchInput = document.getElementById('edit-combine');
     searchBlock.classList.toggle('opened');
+    if (searchBlock.classList.contains('opened')) {
+
+        var changeFocus = setTimeout(setFocus, 3000);
+
+        function setFocus() {
+            console.log('setting focus!');
+            searchInput.focus();
+        }
+
+    } else clearInterval(changeFocus);
 }
 
 function stopPropagation(event) {
